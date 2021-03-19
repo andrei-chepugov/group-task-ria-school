746b5dbd8c47d28d44d9715f78f15976e2d489c9
andrei <andrei.chepugov@gmail.com>
2021-03-19 23:27:38 +0200

    chore. delete "/" and remove empty line

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,14 +2,13 @@ const Router = require('koa-router'),
     KoaBody = require('koa-body'),
     clickhouse = require('../controllers/clickhouseController')
 
-
 const router = new Router();
 
 router
     .get('/databases', clickhouse.getDatabasesList)
     .get('/databases/:name/tables', clickhouse.getDatabaseTable)
     .get('/databases/:name/tables/:tableName/fields', clickhouse.getTableFields)
-    .post('/report/', KoaBody(), clickhouse.createReport);
+    .post('/report', KoaBody(), clickhouse.createReport);
 
 module.exports = {
     routes() {
