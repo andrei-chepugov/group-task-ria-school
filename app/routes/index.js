84b5a4c1b35bf5b2defa735d22244fea37fd667c
andrei <andrei.chepugov@gmail.com>
2021-03-27 03:24:37 +0200

    refactor. add routes

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,21 +1,28 @@
 const Router = require('koa-router'),
     KoaBody = require('koa-body'),
-    schemaController = require('../controllers/schemaController'),
+    adminController = require('../controllers/adminController'),
+    schemaAdminController = require('../controllers/schemaAdminController'),
     userController = require('../controllers/userController'),
+    schemaController = require('../controllers/schemaController'),
     reportController = require('../controllers/reportController')
 
 const router = new Router();
 
 router
-    .get('/admin/databases', schemaController.getDatabasesList)
-    .get('/databases', schemaController.getDatabasesListForUser)
-    .get('/admin/databases/:name/tables', schemaController.getDatabaseTable)
-    .get('/databases/:name/tables', schemaController.getDatabaseTable) // in progress...
-    .get('/databases/:name/tables/:tableName/fields', schemaController.getTableFields)
-    .get('/reports', reportController.getReportsList)
-    .post('/report', KoaBody(), reportController.createReport)
+    .get('/admin/databases', schemaAdminController.getDatabasesList) // for admin
+    .get('/admin/databases/:name/tables', schemaAdminController.getDatabaseTable) // for admin
+    .get('/admin/access-rights/:user/:table', schemaAdminController.setAccessRightsUserForTable) // for admin, in progress...
+    .get('/admin/delete/:id', adminController.deleteUser) // for admin
+    .get('/user', userController.getUserByToken) // for user
+    .get('/logout', KoaBody(), userController.getLogoutUserByToken) // Set-Cookie
+    .get('/databases', schemaController.getDatabasesList) // for user
+    .get('/databases/:name/tables', schemaController.getDatabaseTable) // for user
+    .get('/databases/:name/tables/:tableName/fields', schemaController.getTableFields) // for user
+    .get('/reports', reportController.getReportsList) // for admin & user
+    .post('/admin/update', KoaBody(), adminController.updateUser) // for admin, in progress...
+    .post('/sign-in', KoaBody(), userController.createUser) // Set-Cookie
     .post('/login', KoaBody(), userController.getLoginUser) // Set-Cookie
-    .post('/sign-in', KoaBody(), userController.createUser);
+    .post('/report', KoaBody(), reportController.createReport); // for user
 
 module.exports = {
     routes() {
