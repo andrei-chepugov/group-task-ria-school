37633b68deac208f3590a460ba75cf3fd0c4263d
andrei <andrei.chepugov@gmail.com>
2021-03-28 02:59:40 +0200

    hotfix. sql

--- app/managers/queries/initDBClickhouse/initReportsHistory.sql
+++ app/managers/queries/initDBClickhouse/initReportsHistory.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS reports.history
     `id_user`    String,
     `id_report`  UUID default generateUUIDv4(),
     `name`       String,
-    `isSave`     UInt8,
+    `isSave`     String,
     `created_at` Date default now(),
     `request`    String
 ) ENGINE = MergeTree(created_at, id_report, 8192);
